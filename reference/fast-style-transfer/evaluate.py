3a1e3d498bd6a506ce065dbe05d3dbefde8f46c8
sungjunyoung <wnsdud1861@gmail.com>
2017-09-27 00:52:02 +0900

    google code convention 적용
    —
    Redmine Issue Progress / ID / Time :  30% #23 @30m
    —
    What :
    - reference 폴더 내부의 fast-style-transfer 프로젝트를 google 코드컨벤션에 맞추어 변경
    
    Why :
    - 팀원간의 원활한 협업을 위함
    —
    *Signed-off-by: Junyoung, Sung <wnsdud1861@gmail.com>*

--- reference/fast-style-transfer/evaluate.py
+++ reference/fast-style-transfer/evaluate.py
@@ -17,41 +17,53 @@ DEVICE = '/gpu:0'
 
 
 def from_pipe(opts):
-    command = ["ffprobe",
-               '-v', "quiet",
-               '-print_format', 'json',
-               '-show_streams', opts.in_path]
+    command = [
+        "ffprobe", '-v', "quiet", '-print_format', 'json', '-show_streams',
+        opts.in_path
+    ]
 
     info = json.loads(str(subprocess.check_output(command), encoding="utf8"))
     width = int(info["streams"][0]["width"])
     height = int(info["streams"][0]["height"])
     fps = round(eval(info["streams"][0]["r_frame_rate"]))
 
-    command = ["ffmpeg",
-               '-loglevel', "quiet",
-               '-i', opts.in_path,
-               '-f', 'image2pipe',
-               '-pix_fmt', 'rgb24',
-               '-vcodec', 'rawvideo', '-']
+    command = [
+        "ffmpeg", '-loglevel', "quiet", '-i', opts.in_path, '-f', 'image2pipe',
+        '-pix_fmt', 'rgb24', '-vcodec', 'rawvideo', '-'
+    ]
 
-    pipe_in = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=10 ** 9, stdin=None, stderr=None)
+    pipe_in = subprocess.Popen(
+        command, stdout=subprocess.PIPE, bufsize=10**9, stdin=None, stderr=None)
 
-    command = ["ffmpeg",
-               '-loglevel', "info",
-               '-y',  # (optional) overwrite output file if it exists
-               '-f', 'rawvideo',
-               '-vcodec', 'rawvideo',
-               '-s', str(width) + 'x' + str(height),  # size of one frame
-               '-pix_fmt', 'rgb24',
-               '-r', str(fps),  # frames per second
-               '-i', '-',  # The imput comes from a pipe
-               '-an',  # Tells FFMPEG not to expect any audio
-               '-c:v', 'libx264',
-               '-preset', 'slow',
-               '-crf', '18',
-               opts.out]
+    command = [
+        "ffmpeg",
+        '-loglevel',
+        "info",
+        '-y',  # (optional) overwrite output file if it exists
+        '-f',
+        'rawvideo',
+        '-vcodec',
+        'rawvideo',
+        '-s',
+        str(width) + 'x' + str(height),  # size of one frame
+        '-pix_fmt',
+        'rgb24',
+        '-r',
+        str(fps),  # frames per second
+        '-i',
+        '-',  # The imput comes from a pipe
+        '-an',  # Tells FFMPEG not to expect any audio
+        '-c:v',
+        'libx264',
+        '-preset',
+        'slow',
+        '-crf',
+        '18',
+        opts.out
+    ]
 
-    pipe_out = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=None, stderr=None)
+    pipe_out = subprocess.Popen(
+        command, stdin=subprocess.PIPE, stdout=None, stderr=None)
     g = tf.Graph()
     soft_config = tf.ConfigProto(allow_soft_placement=True)
     soft_config.gpu_options.allow_growth = True
@@ -59,8 +71,8 @@ def from_pipe(opts):
     with g.as_default(), g.device(opts.device), \
          tf.Session(config=soft_config) as sess:
         batch_shape = (opts.batch_size, height, width, 3)
-        img_placeholder = tf.placeholder(tf.float32, shape=batch_shape,
-                                         name='img_placeholder')
+        img_placeholder = tf.placeholder(
+            tf.float32, shape=batch_shape, name='img_placeholder')
         preds = transform.net(img_placeholder)
         saver = tf.train.Saver()
         if os.path.isdir(opts.checkpoint):
@@ -89,8 +101,10 @@ def from_pipe(opts):
                         last = True
                         X = X[:count]
                         batch_shape = (count, height, width, 3)
-                        img_placeholder = tf.placeholder(tf.float32, shape=batch_shape,
-                                                     name='img_placeholder')
+                        img_placeholder = tf.placeholder(
+                            tf.float32,
+                            shape=batch_shape,
+                            name='img_placeholder')
                         preds = transform.net(img_placeholder)
                     break
 
@@ -110,9 +124,10 @@ def from_pipe(opts):
                         pipe_out.stdin.write(img)
                     except IOError as err:
                         ffmpeg_error = pipe_out.stderr.read()
-                        error = (str(err) + ("\n\nFFMPEG encountered"
-                                             "the following error while writing file:"
-                                             "\n\n %s" % ffmpeg_error))
+                        error = (str(err) +
+                                 ("\n\nFFMPEG encountered"
+                                  "the following error while writing file:"
+                                  "\n\n %s" % ffmpeg_error))
                         read_input = False
                         print(error)
         pipe_out.terminate()
@@ -121,6 +136,7 @@ def from_pipe(opts):
         pipe_in.stdout.close()
         del pipe_in
         del pipe_out
+
 
 # get img_shape
 def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
@@ -141,8 +157,8 @@ def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
     with g.as_default(), g.device(device_t), \
             tf.Session(config=soft_config) as sess:
         batch_shape = (batch_size,) + img_shape
-        img_placeholder = tf.placeholder(tf.float32, shape=batch_shape,
-                                         name='img_placeholder')
+        img_placeholder = tf.placeholder(
+            tf.float32, shape=batch_shape, name='img_placeholder')
 
         preds = transform.net(img_placeholder)
         saver = tf.train.Saver()
@@ -155,12 +171,12 @@ def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
         else:
             saver.restore(sess, checkpoint_dir)
 
-        num_iters = int(len(paths_out)/batch_size)
+        num_iters = int(len(paths_out) / batch_size)
         for i in range(num_iters):
             pos = i * batch_size
-            curr_batch_out = paths_out[pos:pos+batch_size]
+            curr_batch_out = paths_out[pos:pos + batch_size]
             if is_paths:
-                curr_batch_in = data_in[pos:pos+batch_size]
+                curr_batch_in = data_in[pos:pos + batch_size]
                 X = np.zeros(batch_shape, dtype=np.float32)
                 for j, path_in in enumerate(curr_batch_in):
                     img = get_img(path_in)
@@ -169,24 +185,33 @@ def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
                         'Resize images or use --allow-different-dimensions.'
                     X[j] = img
             else:
-                X = data_in[pos:pos+batch_size]
+                X = data_in[pos:pos + batch_size]
 
-            _preds = sess.run(preds, feed_dict={img_placeholder:X})
+            _preds = sess.run(preds, feed_dict={img_placeholder: X})
             for j, path_out in enumerate(curr_batch_out):
                 save_img(path_out, _preds[j])
-                
-        remaining_in = data_in[num_iters*batch_size:]
-        remaining_out = paths_out[num_iters*batch_size:]
+
+        remaining_in = data_in[num_iters * batch_size:]
+        remaining_out = paths_out[num_iters * batch_size:]
     if len(remaining_in) > 0:
-        ffwd(remaining_in, remaining_out, checkpoint_dir, 
-            device_t=device_t, batch_size=1)
+        ffwd(
+            remaining_in,
+            remaining_out,
+            checkpoint_dir,
+            device_t=device_t,
+            batch_size=1)
+
 
 def ffwd_to_img(in_path, out_path, checkpoint_dir, device='/cpu:0'):
     paths_in, paths_out = [in_path], [out_path]
     ffwd(paths_in, paths_out, checkpoint_dir, batch_size=1, device_t=device)
 
-def ffwd_different_dimensions(in_path, out_path, checkpoint_dir, 
-            device_t=DEVICE, batch_size=4):
+
+def ffwd_different_dimensions(in_path,
+                              out_path,
+                              checkpoint_dir,
+                              device_t=DEVICE,
+                              batch_size=4):
     in_path_of_shape = defaultdict(list)
     out_path_of_shape = defaultdict(list)
     for i in range(len(in_path)):
@@ -197,38 +222,61 @@ def ffwd_different_dimensions(in_path, out_path, checkpoint_dir,
         out_path_of_shape[shape].append(out_image)
     for shape in in_path_of_shape:
         print('Processing images of shape %s' % shape)
-        ffwd(in_path_of_shape[shape], out_path_of_shape[shape], 
-            checkpoint_dir, device_t, batch_size)
+        ffwd(in_path_of_shape[shape], out_path_of_shape[shape], checkpoint_dir,
+             device_t, batch_size)
+
 
 def build_parser():
     parser = ArgumentParser()
-    parser.add_argument('--checkpoint', type=str,
-                        dest='checkpoint_dir',
-                        help='dir or .ckpt file to load checkpoint from',
-                        metavar='CHECKPOINT', required=True)
+    parser.add_argument(
+        '--checkpoint',
+        type=str,
+        dest='checkpoint_dir',
+        help='dir or .ckpt file to load checkpoint from',
+        metavar='CHECKPOINT',
+        required=True)
 
-    parser.add_argument('--in-path', type=str,
-                        dest='in_path',help='dir or file to transform',
-                        metavar='IN_PATH', required=True)
+    parser.add_argument(
+        '--in-path',
+        type=str,
+        dest='in_path',
+        help='dir or file to transform',
+        metavar='IN_PATH',
+        required=True)
 
     help_out = 'destination (dir or file) of transformed file or files'
-    parser.add_argument('--out-path', type=str,
-                        dest='out_path', help=help_out, metavar='OUT_PATH',
-                        required=True)
+    parser.add_argument(
+        '--out-path',
+        type=str,
+        dest='out_path',
+        help=help_out,
+        metavar='OUT_PATH',
+        required=True)
 
-    parser.add_argument('--device', type=str,
-                        dest='device',help='device to perform compute on',
-                        metavar='DEVICE', default=DEVICE)
+    parser.add_argument(
+        '--device',
+        type=str,
+        dest='device',
+        help='device to perform compute on',
+        metavar='DEVICE',
+        default=DEVICE)
 
-    parser.add_argument('--batch-size', type=int,
-                        dest='batch_size',help='batch size for feedforwarding',
-                        metavar='BATCH_SIZE', default=BATCH_SIZE)
+    parser.add_argument(
+        '--batch-size',
+        type=int,
+        dest='batch_size',
+        help='batch size for feedforwarding',
+        metavar='BATCH_SIZE',
+        default=BATCH_SIZE)
 
-    parser.add_argument('--allow-different-dimensions', action='store_true',
-                        dest='allow_different_dimensions', 
-                        help='allow different image dimensions')
+    parser.add_argument(
+        '--allow-different-dimensions',
+        action='store_true',
+        dest='allow_different_dimensions',
+        help='allow different image dimensions')
 
     return parser
+
 
 def check_opts(opts):
     exists(opts.checkpoint_dir, 'Checkpoint not found!')
@@ -236,6 +284,7 @@ def check_opts(opts):
     if os.path.isdir(opts.out_path):
         exists(opts.out_path, 'out dir not found!')
         assert opts.batch_size > 0
+
 
 def main():
     parser = build_parser()
@@ -249,18 +298,27 @@ def main():
         else:
             out_path = opts.out_path
 
-        ffwd_to_img(opts.in_path, out_path, opts.checkpoint_dir,
-                    device=opts.device)
+        ffwd_to_img(
+            opts.in_path, out_path, opts.checkpoint_dir, device=opts.device)
     else:
         files = list_files(opts.in_path)
-        full_in = [os.path.join(opts.in_path,x) for x in files]
-        full_out = [os.path.join(opts.out_path,x) for x in files]
+        full_in = [os.path.join(opts.in_path, x) for x in files]
+        full_out = [os.path.join(opts.out_path, x) for x in files]
         if opts.allow_different_dimensions:
-            ffwd_different_dimensions(full_in, full_out, opts.checkpoint_dir, 
-                    device_t=opts.device, batch_size=opts.batch_size)
-        else :
-            ffwd(full_in, full_out, opts.checkpoint_dir, device_t=opts.device,
-                    batch_size=opts.batch_size)
+            ffwd_different_dimensions(
+                full_in,
+                full_out,
+                opts.checkpoint_dir,
+                device_t=opts.device,
+                batch_size=opts.batch_size)
+        else:
+            ffwd(
+                full_in,
+                full_out,
+                opts.checkpoint_dir,
+                device_t=opts.device,
+                batch_size=opts.batch_size)
+
 
 if __name__ == '__main__':
     main()
--- reference/fast-style-transfer/src/vgg.py
+++ reference/fast-style-transfer/src/vgg.py
@@ -5,23 +5,16 @@ import numpy as np
 import scipy.io
 import pdb
 
-MEAN_PIXEL = np.array([ 123.68 ,  116.779,  103.939])
+MEAN_PIXEL = np.array([123.68, 116.779, 103.939])
+
 
 def net(data_path, input_image):
-    layers = (
-        'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
-
-        'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
-
-        'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
-        'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
-
-        'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
-        'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
-
-        'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
-        'relu5_3', 'conv5_4', 'relu5_4'
-    )
+    layers = ('conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1', 'conv2_1',
+              'relu2_1', 'conv2_2', 'relu2_2', 'pool2', 'conv3_1', 'relu3_1',
+              'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3', 'conv3_4', 'relu3_4',
+              'pool3', 'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
+              'relu4_3', 'conv4_4', 'relu4_4', 'pool4', 'conv5_1', 'relu5_1',
+              'conv5_2', 'relu5_2', 'conv5_3', 'relu5_3', 'conv5_4', 'relu5_4')
 
     data = scipy.io.loadmat(data_path)
     mean = data['normalization'][0][0][0]
@@ -50,14 +43,14 @@ def net(data_path, input_image):
 
 
 def _conv_layer(input, weights, bias):
-    conv = tf.nn.conv2d(input, tf.constant(weights), strides=(1, 1, 1, 1),
-            padding='SAME')
+    conv = tf.nn.conv2d(
+        input, tf.constant(weights), strides=(1, 1, 1, 1), padding='SAME')
     return tf.nn.bias_add(conv, bias)
 
 
 def _pool_layer(input):
-    return tf.nn.max_pool(input, ksize=(1, 2, 2, 1), strides=(1, 2, 2, 1),
-            padding='SAME')
+    return tf.nn.max_pool(
+        input, ksize=(1, 2, 2, 1), strides=(1, 2, 2, 1), padding='SAME')
 
 
 def preprocess(image):
